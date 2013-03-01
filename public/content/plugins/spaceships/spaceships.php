d4f78c22ab382469d0a5527bfebd489ea158b1f0
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2013-03-01 10:48:17 -0500

    Made spaceships plugin OOP but all in one file

--- public/content/plugins/spaceships/spaceships.php
+++ public/content/plugins/spaceships/spaceships.php
@@ -10,26 +10,71 @@
  * License: A "Slug" license name e.g. GPL2
  */
 
-function spaceships_register_object()
+class CustomObject
 {
-	$labels = array(
-	    'name' => 'Spaceships',
-	    'singular_name' => 'Spaceship',
-	    'add_new' => 'Add New',
-	    'add_new_item' => 'Add New Spaceship',
-	    'edit_item' => 'Edit Spaceship',
-	    'new_item' => 'New Spaceship',
-	    'all_items' => 'All Spaceships',
-	    'view_item' => 'View Spaceship',
-	    'search_items' => 'Search Spaceships',
-	    'not_found' =>  'No spaceships found',
-	    'not_found_in_trash' => 'No spaceships found in Trash', 
-	    'parent_item_colon' => '',
-	    'menu_name' => 'Spaceships'
-	);
-	register_post_type("spaceships", array(
-		"labels" => $labels,
-		"public" => true
-	));
+	public $name;
+	public $singular_name;
+	public $plural_name;
+
+	public $labels = array();
+	public $options = array();
+
+	public function __construct($name, $options, $labels = array())
+	{
+		$names = $this->validate_name($name);
+		$this->name = $names["default"];
+		$this->singular_name = $names["singular"];
+		$this->plural_name = $names["plural"];
+
+		$this->options = $options;
+
+		$default_labels = $this->getDefaultLabels();
+		$this->labels = $labels + $default_labels;
+
+		add_action("init", array($this, "register"));
+	}
+
+	public function register()
+	{
+		$options = $this->options + array("labels" => $this->labels);
+		// print_r($options); die();
+		register_post_type($this->name, $options);
+	}
+
+	public function validate_name($name)
+	{
+		// Make sure it's a valid post type name, < 20 chars, etc
+		// Replace whitespace with underscores
+		// 
+		// Return an array with all names defined
+		return array(
+			"default" => $name,
+			"singular" => ucfirst(rtrim($name, "s")),
+			"plural" => ucfirst($name)
+		);
+	}
+
+	public function getDefaultLabels()
+	{
+		return array(
+			"name" => $this->plural_name,
+	    	"singular_name" => $this->singular_name,
+			"add_new" => "Add New " . $this->singular_name,
+			"add_new_item" => "Add New " . $this->singular_name,
+			"edit" => "Edit", 
+			"edit_item" => "Edit " . $this->singular_name, 
+			"new_item" => "New " . $this->singular_name, 
+			"view" => "View " . $this->singular_name . " Page", 
+			"view_item" => "View " . $this->singular_name, 
+			"search_items" => "Search " . $this->plural_name, 
+			"not_found" => "No matching " . strtolower( $this->plural_name ) . " found",
+			"not_found_in_trash" => "No " . strtolower( $this->plural_name ) . " found in Trash",
+			"parent_item_colon" => "Parent " . $this->singular_name,
+		);
+	}
 }
-add_action("init", "spaceships_register_object");
+
+
+$spaceships = new CustomObject("spaceships", array("public" => true));
+
+
